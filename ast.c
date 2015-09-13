80fff329222880ac68861bc87510a17df48fdcfb
Michael Gerdes <mgerdes11@gmail.com>
2015-09-13 14:20:04 -0400

    Print s-expr's for the AST

--- ast.c
+++ ast.c
@@ -18,36 +18,42 @@ void print_ast_indented(Node* ast, int tabs) {
         }
     }
     if (ast->kind == KIND_CONSTANT) {
-        printf("CONSTANT -> %d\n", ast->i_value);
+        printf("(CONSTANT %d)", ast->i_value);
     } else if (ast->kind == KIND_FUNC) {
-        printf("FUNCTION -> %s\n", ast->symbol->symbol_name);
+        printf("(FUNCTION %s\n", ast->symbol->symbol_name);
         print_ast_indented(ast->body_node, tabs + 1);
+        printf(")\n");
     } else if (ast->kind == KIND_RETURN) {
-        printf("RETURN\n");
+        printf("(RETURN");
+        printf("\n");
         print_ast_indented(ast->return_node, tabs + 1);
+        printf(")");
     } else if (ast->kind == KIND_ADD) {
-        printf("+\n");
+        printf("(+\n");
         print_ast_indented(ast->left_node, tabs + 1);
+        printf("\n");
         print_ast_indented(ast->right_node, tabs + 1);
+        printf(")");
     } else if (ast->kind == KIND_SUB) {
-        printf("-\n");
+        printf("-");
         print_ast_indented(ast->left_node, tabs + 1);
         print_ast_indented(ast->right_node, tabs + 1);
     } else if (ast->kind == KIND_SYMBOL) {
-        printf("SYMBOL -> %s\n", ast->symbol_name);
+        printf("(SYMBOL %s)", ast->symbol_name);
     } else if (ast->kind == KIND_ASSIGNMENT) {
-        printf("ASSIGNMENT -> %s\n", ast->symbol->symbol_name);
+        printf("(ASSIGNMENT %s\n", ast->symbol->symbol_name);
         print_ast_indented(ast->right_node, tabs + 1);
+        printf(")\n");
     } else if (ast->kind == KIND_BLOCK) {
         print_ast_indented(ast->left_node, tabs);
         if (ast->right_node) {
             print_ast_indented(ast->right_node, tabs);
         }
     } else if (ast->kind == KIND_FUNC_CALL) {
-        printf("FUNCTION_CALL -> %s\n", ast->symbol->symbol_name);
+        printf("(FUNCTION_CALL %s)", ast->symbol->symbol_name);
     }
     else {
-        printf("UNKNOWN KIND - %d\n", ast->kind);
+        printf("(UNKNOWN KIND %d)\n", ast->kind);
     }
 }
 
