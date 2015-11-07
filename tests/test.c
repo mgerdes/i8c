3cdc766b8a8c20516862ddd159f446b8196d3f4e
Michael Gerdes <mgerdes11@gmail.com>
2015-11-07 14:45:04 -0500

    Organized the tests a little bit more

--- tests/test.c
+++ tests/test.c
@@ -40,18 +40,32 @@ struct test_struct {
     int v2;
 };
 
-int main() {
+int add_struct(struct test_struct* h) {
+    return h->v1 + h->v2;
+}
+
+void struct_tests() {
     struct test_struct t;
     struct test_struct* p;
+
     t.v1 = 10;
     t.v2 = 23;
     p = &t;
 
-    expect_equals(t.v1, 10, "Struct member test 1");
-    expect_equals(t.v2, 23, "Struct member test 2");
-    expect_equals(p->v1, 10, "Struct member test 3");
-    expect_equals(p->v2, 23, "Struct member test 4");
+    expect_equals(t.v1, 10, "Struct test 1");
+    expect_equals(t.v2, 23, "Struct test 2");
+    expect_equals(p->v1, 10, "Struct test 3");
+    expect_equals(p->v2, 23, "Struct test 4");
 
+    p->v1 = 20;
+    p->v2 = 125;
+    expect_equals(p->v1, 20, "Struct test 5");
+    expect_equals(p->v2, 125, "Struct test 6");
+
+    expect_equals(add_struct(p), 145, "Struct test 7");
+}
+
+void binary_operator_tests() {
     expect_equals(1 + 1, 2, "Addition test 1");
     expect_equals(0 + 1, 1, "Addition test 2");
     expect_equals(-1 + 1, 0, "Addition test 3");
@@ -73,7 +87,9 @@ int main() {
     expect_equals(2 % 2, 0, "Mod test 2");
     expect_equals(3 % 2, 1, "Mod test 3");
     expect_equals(21 % 5, 1, "Mod test 4");
+}
 
+void function_call_tests() {
     expect_equals(fib(0), 0, "Fibonnaci test 1");    
     expect_equals(fib(1), 1, "Fibonnaci test 2");    
     expect_equals(fib(10), 55, "Fibonnaci test 3");    
@@ -83,6 +99,11 @@ int main() {
     expect_equals(sum(10), 55, "Sum test 3");
 
     expect_equals(zero(), 0, "Function call test 1");
+}
 
+int main() {
+    struct_tests();
+    binary_operator_tests();
+    function_call_tests();
     return 0;
 }
